523333fc5138fa47496ded3532000bf03039a686
andrechalom <andrechalom@gmail.com>
2014-07-09 17:50:50 -0300

    Update and rename VivoMorto.cpp to TWoLife.cpp

--- TWoLife.cpp
+++ TWoLife.cpp
@@ -8,10 +8,9 @@
 
 using namespace std;
 
-/** \file VivoMorto.cpp \brief Arquivo usado na exploração de espaços de parâmetros
+/** \file TWoLife.cpp \brief Arquivo usado na integração R/C
  * 
- * Este arquivo é muito semelhante ao main.cpp, mas ao invés de aceitar parâmetros pela linha de comando, ele gera
- * uma função que pode ser chamada facilmente a partir do R */
+ * Este arquivo define a função TWoLife em C, que será chamada a partir do R */
 
 
 /** Funcao que recebe os parâmetros da simulação e a executa,
@@ -22,13 +21,13 @@ using namespace std;
 //
 // Veja a descrição da classe \ref paisagem para o significado dos parâmetros
 // */
-extern "C" void VivoMorto (double * raio, int * N, double * angulo_visada, double * passo, double * move,
+extern "C" void TWoLife (double * raio, int * N, double * angulo_visada, double * passo, double * move,
 						   double * taxa_basal, double * taxa_morte, double * incl_b, double * incl_d,
 						   int * numb_cells, double * cell_size, int * land_shape, int * density_type, 
 						   double * death_mat, int * bound_condition, double * cover, double * tempo, int * nPop, double * x, double * y)
 {
   GetRNGstate(); /* (mudar para doxygen):  este comando chama o engine de numeros aleatorios do R
-		    Por causa dela  nossa biblioteca VivoMorto nao eh standalone */
+		    Por causa dela  nossa biblioteca nao eh standalone */
 	paisagem* floresta = new paisagem(raio[0], N[0], angulo_visada[0], passo[0], 
 					  move[0], taxa_basal[0], taxa_morte[0], incl_b[0], 
 					  incl_d[0], numb_cells[0], cell_size[0], land_shape[0],
