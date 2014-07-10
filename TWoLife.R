231ebba8e46c29414f22e9610b549b7810e56a4c
Andre Chalom <andrechalom@gmail.com>
2014-07-10 00:08:00 -0300

    Primeira versao de Landscapes

--- TWoLife.R
+++ TWoLife.R
@@ -7,77 +7,117 @@ system("rm TWoLife.{so,o}") #limpa sources velhos
 system ("R CMD SHLIB TWoLife.cpp") ## compila no R
 dyn.load("TWoLife.so") ## carrega os source resultantes como biblioteca dinamica no R
 
+# Generates the landscape with specified conditions. 
+# numb.cells represents both the lenght AND width of the landscape, so numb.cells=100 creates a 100x100 landscape
+# Land.shape can be 0 = XXX or 1 = XXX.
+# Bound.condition can be 0 = XXX or 1 = XXX. The only type supported right now is random. 
+Landscape <- function (numb.cells = 100, cell.size = 1, land.shape = 1, type="random", bound.condition=0, cover=1) {
+	if(cover < 0 || cover > 1) {
+		stop("Error creating landscape. Cover must be between 0 and 1")
+	}
+	if (type != "random") {
+		stop("Only random landscapes are currently implemented")
+	}
+	# scape represents the actual landscape
+	scape <- rep(1, numb.cells*numb.cells)
+	if(cover < 1) {
+		NtoRemove=round((1-cover)*numb.cells*numb.cells);
+		while(NtoRemove>0)
+		{
+			i=round(runif(1,0,numb.cells-1));
+			j=round(runif(1,0,numb.cells-1));
+			# tests to see if this point has already been removed
+			if(scape[numb.cells*j+i] == 1) {
+				NtoRemove = NtoRemove - 1
+				scape[numb.cells*j+i] = 0
+			}
+		}
+	}
+	land <- list(numb.cells = numb.cells, cell.size=cell.size, land.shape=land.shape, type=type, bound.condition=bound.condition, cover=cover, scape=scape)
+	class(land) <- "landscape"
+	return(land)
+}
+
 TWoLife <- function (
-    raio=0.1, 
-    N=80, 
-    AngVis=360, 
-    passo=5, 
-    move=3, 
-    taxa.basal=0.6, 
-    taxa.morte=0.1, 
-    incl.birth=0.5/0.01, 
-    incl.death=0, 
-    numb.cells=200, 
-    cell.size=1, 
-    land.shape=1, 
-    density.type=0,
-    death.mat=7, 
-    bound.condition=0, 
-    cover=1, 
-    tempo=20) 
+					 raio=0.1, 
+					 N=80, 
+					 AngVis=360, 
+					 passo=5, 
+					 move=0.5, 
+					 taxa.basal=0.6, 
+					 taxa.morte=0.1, 
+					 incl.birth=0.5/0.01, 
+					 incl.death=0, 
+					 density.type=0,
+					 death.mat=7, 
+					 landscape,
+					 tempo=20) 
 {
-    saida.C <- .C("TWoLife", as.double(raio), as.integer(N),as.double(AngVis), as.double(passo),
-                  as.double(move), as.double(taxa.basal), as.double(taxa.morte), 
-                  as.double(incl.birth), as.double(incl.death), as.integer(numb.cells), 
-                  as.double(cell.size), as.integer(land.shape), as.integer(density.type), 
-                  as.double(death.mat), as.integer(bound.condition), as.double(cover),
-                  as.double(tempo), as.integer(0),
-                  as.double(rep(0, 5000)), as.double(rep(0,5000)) ## verificar se precisa definir o tamanho e se isto nao dará problemas
-                  )
-    n <- saida.C[[18]]
-    x <- saida.C[[19]]
-    y <- saida.C[[20]]
-    x <- x[1:n]; y <- y[1:n]  
-    return(data.frame(x=x,y=y))
+	if(class(landscape) != "landscape") {
+		stop("Error in function TWoLife: you must provide a valid landscape. See ?Landscape")
+	}
+	saida.C <- .C("TWoLife", as.double(raio), as.integer(N),as.double(AngVis), as.double(passo),
+				  as.double(move), as.double(taxa.basal), as.double(taxa.morte), 
+				  as.double(incl.birth), as.double(incl.death), as.integer(landscape$numb.cells), 
+				  as.double(landscape$cell.size), as.integer(landscape$land.shape), as.integer(density.type), 
+				  as.double(death.mat), as.integer(landscape$bound.condition), as.double(landscape$cover),
+				  as.double(tempo), as.integer(0),
+				  as.double(rep(0, 5000)), as.double(rep(0,5000)) ## verificar se precisa definir o tamanho e se isto nao dará problemas
+				  )
+	n <- saida.C[[18]]
+	x <- saida.C[[19]]
+	y <- saida.C[[20]]
+	x <- x[1:n]; y <- y[1:n]  
+	return(data.frame(x=x,y=y))
 }
 
 ## Um teste rapido
+land <- Landscape(cover=0.5)
 ## Uma rodada: coordenadas dos sobreviventes apos t=20
-teste1 <- TWoLife(raio=0.1,
-                    N=80,
-                    AngVis=360,
-                    passo=5,
-                    move=3,
-                    taxa.basal=0.6,
-                    taxa.morte=0.1, 
-                    incl.birth=0.5/0.01,
-                    incl.death=0,
-                    numb.cells=200,
-                    cell.size=1,
-                    land.shape=1, 
-                    density.type=0,
-                    death.mat=7,
-                    bound.condition=0,
-                    cover=1,
-                    tempo=20)
-plot(teste1, xlim=c(-100,100), ylim=c(-100,100))
-dim(teste1)
+teste <- TWoLife(raio=0.1,
+				  N=80,
+				  AngVis=360,
+				  passo=5,
+				  move=3,
+				  taxa.basal=0.6,
+				  taxa.morte=0.1, 
+				  incl.birth=0.5/0.01,
+				  incl.death=0,
+				  density.type=0,
+				  death.mat=7,
+				  landscape=land,
+				  tempo=10)
+TWoPlot <- function(pop, land) {
+	n = land$numb.cells
+	s <- seq(-n*land$cell.size/2, n*land$cell.size/2, length=n) # creates the x- and y- sequences for image
+	if (sum(land$scape) == n*n) { 
+		color = "gray20"
+	} else {
+		color = c("gray70", "gray20")
+	}
+	image(s, s, matrix(land$scape,ncol=n), col=color)
+	points(pop, pch=4, col=2)
+}
+#plot(teste1, xlim=c(-100,100), ylim=c(-100,100))
+#dim(teste1)
 ## Tamanho de populacao apos t=6 de 100 repeticoes
-pop.size<- numeric()
-for (i in 1:100) 
-  {
-    pop.size[i] = 
-      nrow(
-          TWoLife(raio=0.1, N=80, AngVis=360, passo=5, move=0.1, taxa.basal=0.6,
-                    taxa.morte=0.1, 
-                    incl.birth=0.5/0.01, incl.death=0, numb.cells=200, cell.size=1, land.shape=1, 
-                    density.type=0, death.mat=7,bound.condition=0, cover=1, tempo=6))
-  }
+#pop.size<- numeric()
+#for (i in 1:20) 
+#  {
+#    pop.size[i] = 
+#      nrow(
+#          TWoLife(raio=0.1, N=80, AngVis=360, passo=5, move=0.1, taxa.basal=0.6,
+#                    taxa.morte=0.1, 
+#                    incl.birth=0.5/0.01, incl.death=0, numb.cells=200, cell.size=1, land.shape=1, 
+#                    density.type=0, death.mat=7,bound.condition=0, cover=1, tempo=6))
+#  }
 
-## esperado: capacidade de suporte (numb.cells*cell.size)^2 * densi_max
-## densi_max= (taxa_basal-taxa_morte)/(incl_b+incl_d)
-200^2*(0.6-0.1)/(0.5/.01)
+## esperado: capacidade de suporte 
+Support <- function(taxa.basal=0.6, taxa.morte=0.1, incl.birth=0.5/0.01, 
+					incl.death=0, numb.cells=200, cell.size=2) {
+	densi.max = (taxa.basal-taxa.morte)/(incl.birth+incl.death)
+	return ((numb.cells*cell.size)^2 * densi.max)
+}
 ## Media das simulacoes
-pop.size
-mean(pop.size)
-hist(pop.size)
+#print(pop.size - Support())
+#print(mean(pop.size - Support()))
