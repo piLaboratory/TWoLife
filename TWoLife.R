2e676ddb4e7b4d5d0329ea927e0084db2c210bf7
Felipe <felipe.ernesto.campos@usp.br>
2017-06-14 07:28:31 -0300

    Aprendendo a usar o git :)

--- TWoLife.R
+++ TWoLife.R
@@ -117,7 +117,7 @@ TWoLife <- function (
 }
 npop<-10
  ## Um teste rapido
-  land <- Landscape(cover=1,type="b",cell.size=100, bound.condition=1)#Nao usar bound.condition=0 pq essa parte da apply_boundary terá de ser adaptada
+  land <- Landscape(cover=1,type="b",cell.size=100, bound.condition=1)
  # ## Uma rodada: coordenadas dos sobreviventes apos t=20
  teste <- TWoLife(raio=1560,
  				 N=npop,
