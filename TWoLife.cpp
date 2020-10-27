6e7bafd4664e3d5962145b96f590e12beda4f020
DeFreitasLR <defreitaslr.eco@gmail.com>
2020-10-27 07:55:15 -0300

    Points fixing

--- TWoLife.cpp
+++ TWoLife.cpp
@@ -4,113 +4,167 @@
 #include <cstdlib>
 #include <R.h>
 #include <Rmath.h>
-#include "paisagem.cpp" 
-#include "individuo.cpp" 
+#include "paisagem.cpp"
+#include "individuo.cpp"
 #include <sstream>
 #include <string>
 
 using namespace std;
 
-/** \file TWoLife.cpp \brief Arquivo usado na integração R/C
- * 
- * Este arquivo define a função TWoLife em C, que será chamada a partir do R */
+/** \file TWoLife.cpp \brief File used for R/C++ interaction
+ *
+ * This file defines the TWoLife function in C, to be be called by R */
 
-
-/** Funcao que recebe os parâmetros da simulação e a executa,
- // baseada no texto em http://users.stat.umn.edu/~geyer/rc/
+/** The function recieves the inputed parameters and executes the simulation
+ // Based on the text avaiable on http://users.stat.umn.edu/~geyer/rc/
  //
- // NOTE que funções em C++ precisam ser indicadas como extern "C"
- // para poderem ser acessadas facilmente pela interface do R!
+ // Observe that funtions in C++ need to be called as extern "C"
+ // as to be easely accessd by the R interface
  //
- // Veja a descrição da classe \ref paisagem para o significado dos parâmetros
+ // See the description off the \ref paisagem class for parameter meenings
  // */
-extern "C" void TWoLife (double * raio, int * N, double * angulo_visada, double * passo, double * move,
-						 double * taxa_basal, double * taxa_morte, double * incl_b, double * incl_d,
-						 int * numb_cells, double * cell_size, int * land_shape, int * density_type, 
-						 double * death_mat, int * inipos, int * bound_condition, double * scape, 
-						 double * tempo, int * nPop, double * x, double * y, int * outCode, 
-						 double * genotype_means, double * width_sds, bool * Null, double * m, double * initialPosX, double * initialPosY)
+ extern "C" void TWoLife (double * raio,
+                         int * N,
+                         double * angulo_visada,
+                         double * passo,
+                         double * taxa_move,
+                         double * taxa_basal,
+                         double * taxa_morte,
+                         double * incl_b,
+                         double * incl_d,
+                         int * numb_cells,
+                         double * cell_size,
+                         int * land_shape,
+                         int * density_type,
+                         double * death_mat,
+                         double * move_mat,
+                         int * inipos,
+                         int * bound_condition,
+                         double * scape,
+                         double * tempo,
+                         double * initialPosX,
+                         double * initialPosY,
+                         double * genotype_means,
+                         double  * width_sds,
+                         int * points,
+                         bool * Null,// Output "inputs"
+                         int * nPop,
+                         double * x,
+                         double * y,
+                         double * m,
+                         char ** outCode)
+ {
+   // Names the output file name
+   stringstream tmps; // Creates an object of the stringstream to recieve the output name
+   tmps<<outCode[0]; // Stores the inputed name in the stringstream object
+   string fileNAME = tmps.str(); // Stores the desired file name as Filename
+   
+   GetRNGstate(); /* This comands calls R's random number generator */
+
+/** Generates an object of the paisagem class using the inputed simulation parameters  */
+paisagem* floresta = new paisagem(raio[0],
+                                  N[0],
+                                   angulo_visada[0],
+                                                passo[0],
+                                                     taxa_move[0],
+                                                              taxa_basal[0],
+                                                                        taxa_morte[0],
+                                                                                  incl_b[0],
+                                                                                        incl_d[0],
+                                                                                              numb_cells[0],
+                                                                                                        cell_size[0],
+                                                                                                                 land_shape[0],
+                                                                                                                           density_type[0],
+                                                                                                                                       death_mat[0],
+                                                                                                                                                move_mat[0],
+                                                                                                                                                        inipos[0],
+                                                                                                                                                              bound_condition[0],
+                                                                                                                                                                             scape,
+                                                                                                                                                                             initialPosX,
+                                                                                                                                                                             initialPosY,
+                                                                                                                                                                             genotype_means,
+                                                                                                                                                                             width_sds,
+                                                                                                                                                                             points,
+                                                                                                                                                                             Null[0]);
+
+ofstream outputSIM; // Creates ofstream for the output file
+outputSIM.open(fileNAME.c_str()); // Opens the ofstream for the output file
+
+// Outputs the simulation starting conditions
+outputSIM << "Lado: " << floresta->get_tamanho() << endl; // Outputs the lenght of the landscape side
+outputSIM << "Tamanho pixel: " << cell_size[0] << endl; // Outputs the lenght of a pixel side
+outputSIM << "Pixels lado: " << numb_cells[0] << endl; // Outputs the number of pixels on the landscapeside
+outputSIM << "N fragmentos: " << floresta->get_numb_patches() << endl; // Outputs the number of fragments on the landscape
+outputSIM << "Area fragmentos: "; // Start Outputing the area of each of the fragments
+
+
+double sum = 0;// Creates a accumulator variable
+//outputSIM << floresta->get_patch_area(0) << " "; //Imprimir area matriz?
+for (int i = 1; i <= floresta->get_numb_patches(); i++) // Passes through each patch
 {
-	// This sequence creates an attribute containing the output file name. The template is output-00000.txt.
-	string fileNAME = "output-00000.txt";
-	stringstream tmps;
-	tmps<<outCode[0];
-	string addToName = tmps.str();
-	int fnSize = fileNAME.size();
-    int tmpsSize = addToName.size();
-	fileNAME.erase(fileNAME.begin()+fnSize-4-tmpsSize,fileNAME.begin()+fnSize-4);
-	fileNAME.insert(fnSize-4-tmpsSize,addToName);
-	
-	GetRNGstate(); /* (mudar para doxygen):  este comando chama o engine de numeros aleatorios do R
-					Por causa dela nossa biblioteca nao eh standalone */
-	
-	paisagem* floresta = new paisagem(raio[0], N[0], angulo_visada[0], passo[0], 
-									  move[0], taxa_basal[0], taxa_morte[0], incl_b[0], 
-									  incl_d[0], numb_cells[0], cell_size[0], land_shape[0],
-									  density_type[0], death_mat[0], inipos[0], bound_condition[0], 
-									  scape, genotype_means, width_sds, Null[0], initialPosX, initialPosY);
-	
-	ofstream outputSIM; // ofstream for the output file
-	outputSIM.open(fileNAME.c_str());
+  sum += floresta->get_patch_area(i); // Sums a patch area to the acumulator
+  outputSIM << floresta->get_patch_area(i) << " "; // Outputs the area of each fragment
+}
 
-	outputSIM << "Lado: " << floresta->get_tamanho() << endl;
-	outputSIM << "Tamanho pixel: " << cell_size[0] << endl;
-	outputSIM << "Pixels lado: " << numb_cells[0] << endl;
-	outputSIM << "N fragmentos: " << floresta->get_numb_patches() << endl;
-	outputSIM << "Area fragmentos: ";
-	double sum = 0;
-	//outputSIM << floresta->get_patch_area(0) << " "; //Imprimir area matriz?
-	for (int i = 1; i <= floresta->get_numb_patches(); i++)
-	{
-		sum += floresta->get_patch_area(i);
-		outputSIM << floresta->get_patch_area(i) << " ";		
-	}
-	outputSIM << "\nProporção de habitat: " << sum/(floresta->get_tamanho()*floresta->get_tamanho())<< "\n\n\n\n";
+outputSIM << "\nProporção de habitat: " << sum/(floresta->get_tamanho()*floresta->get_tamanho())<< "\n\n\n\n"; // Outputs the proportion of the landscape that is habitable
 
-	
 
-	for(unsigned int i=0; i<floresta->conta_individuos();i++)
-	{
-		outputSIM << floresta->tempo_do_mundo << " " << floresta->get_individuos(i)->get_id() << " "  << floresta->get_individuos(i)->get_patch() << " " << floresta->get_individuos(i)->get_x() << " " << floresta->get_individuos(i)->get_y() << " " << floresta->get_individuos(i)->get_genotype_mean() << endl;
-	}
-	while (floresta->tempo_do_mundo < tempo[0] && floresta->conta_individuos() > 0)
-	{
-		int ind_neo = floresta->sorteia_individuo();
-		int acao = floresta->sorteia_acao(ind_neo);
-		floresta->atualiza_tempo(ind_neo);
 
-		int ID_neo = floresta->get_individuos(ind_neo)->get_id();
-		double x_neo = floresta->get_individuos(ind_neo)->get_x();
-		double y_neo = floresta->get_individuos(ind_neo)->get_y();
-		int patch_neo = floresta->get_individuos(ind_neo)->get_patch();
-		double m_neo = floresta->get_individuos(ind_neo)->get_genotype_mean();
-		
-		bool emigrou = floresta->realiza_acao(acao, ind_neo);
-		floresta->update();
 
-		if(acao==2 && !emigrou)
-		{
-			x_neo = floresta->get_individuos(ind_neo)->get_x();
-			y_neo = floresta->get_individuos(ind_neo)->get_y();
-			patch_neo = floresta->get_individuos(ind_neo)->get_patch();
-		}
-		else if(acao==2 && emigrou)
-			acao = 3;
+for(unsigned int i=0; i<floresta->conta_individuos();i++) // Passes through each starting individual
+{
+  // Outputs all the starting individuals and their informations
+  outputSIM << floresta->tempo_do_mundo << " " << floresta->get_individuos(i)->get_id() << " "  << floresta->get_individuos(i)->get_patch() << " " << floresta->get_individuos(i)->get_x() << " " << floresta->get_individuos(i)->get_y() << " " << floresta->get_individuos(i)->get_genotype_mean() << endl;
+}
 
-		outputSIM << floresta->tempo_do_mundo << " " << acao << " " << ID_neo << " "  << patch_neo << " " << x_neo << " " << y_neo << " " << m_neo << endl;
-	}
-	outputSIM<< "EOF\n";
-	outputSIM.close(); //end of output file
-	
-	
-	*nPop = floresta->conta_individuos();
-	for (int i =0; i < *nPop; i ++) {
-		x[i] = floresta->get_individuos(i)->get_x();
-		y[i] = floresta->get_individuos(i)->get_y();
-		m[i] = floresta->get_individuos(i)->get_genotype_mean();
-	} //DUVIDA: porque x[i] e y[i] nao tem asterisco antes?
-	delete floresta;
-	PutRNGstate();
+/** Performs the simulation  */
+while (floresta->tempo_do_mundo < tempo[0] && floresta->conta_individuos() > 0 && floresta->conta_individuos() < 2000) // Performs the simulation until the maximum desired time is reached
+{
+  int ind_neo = floresta->sorteia_individuo(); // Drafts an individual to perform an action
+  int acao = floresta->sorteia_acao(ind_neo); // Drafts an action for the individual to perform
+  floresta->atualiza_tempo(ind_neo);  // Updstes the world Clock
+  
+  int ID_neo = floresta->get_individuos(ind_neo)->get_id(); // Obtains the ID of the drafted individual
+  double x_neo = floresta->get_individuos(ind_neo)->get_x(); // Obtains the X coordinate of the drafted individual
+  double y_neo = floresta->get_individuos(ind_neo)->get_y(); // Obtains the Y coordinate of the drafted individual
+  int patch_neo = floresta->get_individuos(ind_neo)->get_patch(); // Obtains the curent patch of the drafted individual
+  double m_neo = floresta->get_individuos(ind_neo)->get_genotype_mean();
+  
+  bool emigrou = floresta->realiza_acao(acao, ind_neo); // Makes the drafted individual perform the chosen action
+  
+  if(acao==2 && emigrou) //Checks if the action performed was a migration event AND the drafted individual left the landscape boundary
+    floresta->update(0, ind_neo); // Updates the drafted individual migration event as a death event
+  else // Comtemplates all other cases
+    floresta->update(acao, ind_neo); // Updates the individual according to the performed action
+  
+  if(acao==2 && !emigrou) //Checks if the action performed was a migration event AND the drafted individuals new location is within the landscape boundary
+  {
+    x_neo = floresta->get_individuos(ind_neo)->get_x(); // Obtains the individuals current x coordinate
+    y_neo = floresta->get_individuos(ind_neo)->get_y(); // Obtains the individuals current y coordinate
+    patch_neo = floresta->get_individuos(ind_neo)->get_patch(); // Obtains the individuals current patch
+  }
+  else if(acao==2 && emigrou) //Checks if the action performed was a migration event AND the drafted individual left the landscape boundary
+    acao = 3; // Sets the output entry of the individual as an emigration event
+  
+  // Outputs the actions performed by the drafted individual
+  outputSIM << floresta->tempo_do_mundo << " " << acao << " " << ID_neo << " "  << patch_neo << " " << x_neo << " " << y_neo << " " << m_neo << endl;
 }
 
 
+outputSIM<< "EOF\n";// Outbuts a warning stating the simulation was run to completion
+outputSIM.close(); //Closes the output file
+
+//Outputs the remaining individuals
+*nPop = floresta->conta_individuos(); // Checks how many individuals are left
+for (int i =0; i < *nPop; i ++) { // Passes through each remaining individual
+  x[i] = floresta->get_individuos(i)->get_x(); // Outputs its x coordinate
+  y[i] = floresta->get_individuos(i)->get_y(); // Outputs its y coordinate
+  m[i] = floresta->get_individuos(i)->get_genotype_mean();
+}
+
+delete floresta;
+
+PutRNGstate();
+ }
+ 
+ 
--- individuo.cpp
+++ individuo.cpp
@@ -16,10 +16,10 @@ unsigned long individuo::MAXID = 0;
  * passados no construtor, mas vai ser expandida para compreender mais casos (\ref TBI). */
 class individuo_exception: public exception
 {
-	virtual const char* what() const throw()
-	{
-		return "Aconteceu uma exceção na classe indivíduo!";
-	}
+  virtual const char* what() const throw()
+  {
+    return "Aconteceu uma exceção na classe indivíduo!";
+  }
 } myex;
 
 individuo::individuo(double x,
@@ -29,90 +29,99 @@ individuo::individuo(double x,
                      double orientacao,
                      double angulo_visada,
                      double passo,
-                     double move,
+                     double taxa_move,
                      double raio,
                      double taxa_basal,
-                     int semente, //retirar int semente 
+                     int semente, //retirar int semente
                      double incl_b,
                      double incl_d,
                      double death_mat,
+                     double move_mat,
                      int dens_type,
                      vector <double> genotype,
-                     vector <double> width
-					 ):
-	id(++MAXID), // pega o próximo ID livre
-	x(x),
-	y(y),
-	especie(especie),
-	taxa_morte(taxa_morte),
-	move(move),
-	passo(passo),
-	orientacao(orientacao),
-	ang_visada(angulo_visada),
-	raio(raio),
-	taxa_basal(taxa_basal),
-	semente(semente),
-	incl_birth(incl_b),
-	incl_death(incl_d),
-	const_d_matrix(death_mat),
-	dens_type(dens_type)
+                     vector <double> width,
+                     int points
+                       
+):
+  id(++MAXID), // pega o próximo ID livre
+  x(x),
+  y(y),
+  especie(especie),
+  taxa_morte(taxa_morte),
+  orientacao(orientacao),
+  ang_visada(angulo_visada),
+  passo(passo),
+  taxa_move(taxa_move),
+  move(taxa_move),
+  raio(raio),
+  taxa_basal(taxa_basal),
+  semente(semente),
+  incl_birth(incl_b),
+  incl_death(incl_d),
+  const_d_matrix(death_mat),
+  const_m_matrix(move_mat),
+  dens_type(dens_type),
+  points(points)
 {
-	if (taxa_morte < 0) throw myex;
-	if (passo < 0) throw myex;
-	if (move < 0) throw myex;
-	if (raio < 0) throw myex;
-	if (taxa_basal < 0) throw myex;	
-	
-	if(incl_birth!=0 && incl_death!=0)
-	{
-		this->densi_max = (taxa_basal-taxa_morte)/(incl_b+incl_d);
-		this->birth_death_eq = taxa_morte+incl_d*((taxa_basal-taxa_morte)/(incl_b+incl_d));
-	}
-	
-	this->genotype_mean = genotype;
-	this->width_sd = width;
-	
-	this->rdn_noise = 0;
-	
-	for (int i=0; i<this->genotype_mean.size(); i++) {
-	  
-	  this->env_optimum.push_back(this->rdn_noise+this->genotype_mean[i]);
-	}
-	
-	this->points = 10;
+  if (taxa_morte < 0) throw myex;
+  if (passo < 0) throw myex;
+  if (taxa_move < 0) throw myex;
+  if (raio < 0) throw myex;
+  if (taxa_basal < 0) throw myex;
+  
+  if(incl_birth!=0 && incl_death!=0)
+  {
+    this->densi_max = (taxa_basal-taxa_morte)/(incl_b+incl_d);
+    this->birth_death_eq = taxa_morte+incl_d*((taxa_basal-taxa_morte)/(incl_b+incl_d));
+  }
+  
+  this->genotype_mean = genotype;
+  
+  this->width_sd = width;
+  
+  this->rdn_noise = 0;
+  
+  for (int i=0; i<this->genotype_mean.size(); i++) {
+    
+    this->env_optimum.push_back(this->rdn_noise+this->genotype_mean[i]);
+  }
 }
 
 
 /** \brief função para reprodução assexuada de um indivíduo
- * 
+ *
  * Faz uma cópia do indivíduo pai. Recebe um ponteiro dereferenciado (nomeado rhs) e usa isto para
  * Criar novo individuo com os mesmos valores de atributo do pai, exceto
  *  - id (veja \ref individuo::get_id)
  *  - vizinhos (veja \ref individuo::set_vizinhos)
- *  - tempo para evento (veja \ref individuo::get_tempo) 
- * Usa notacao :atributo(valor) ao inves de atribuicão. 
+ *  - tempo para evento (veja \ref individuo::get_tempo)
+ * Usa notacao :atributo(valor) ao inves de atribuicão.
  * Funcao chamada por paisagem::realiza_acao() quando a ação é um nascimentto
  * @param rhs ponteiro dereferenciado para o pai */
-individuo::individuo(const individuo& rhs)
-    :id(++MAXID),
-      x(rhs.x),
-      y(rhs.y),
-      especie(rhs.especie),
-      taxa_morte(rhs.taxa_morte),
-      move(rhs.move),
-      passo(rhs.passo),
-      ang_visada(rhs.ang_visada),
-      raio(rhs.raio),
-      taxa_basal(rhs.taxa_basal),
-      tipo_habitat(rhs.tipo_habitat),
-      semente(rhs.semente),
-	  incl_birth(rhs.incl_birth),
-	  incl_death(rhs.incl_death),
-	  const_d_matrix(rhs.const_d_matrix),
-	  dens_type(rhs.dens_type),
-	  birth_death_eq(rhs.birth_death_eq)
-	  
-{ 
+individuo::individuo(const individuo& rhs):
+  id(++MAXID),
+  x(rhs.x),
+  y(rhs.y),
+  especie(rhs.especie),
+  taxa_morte(rhs.taxa_morte),
+  ang_visada(rhs.ang_visada),
+  passo(rhs.passo),
+  taxa_move(rhs.taxa_move),
+  move(rhs.taxa_move),
+  raio(rhs.raio),
+  taxa_basal(rhs.taxa_basal),
+  semente(rhs.semente),
+  incl_birth(rhs.incl_birth),
+  incl_death(rhs.incl_death),
+  const_d_matrix(rhs.const_d_matrix),
+  const_m_matrix(rhs.const_m_matrix),
+  dens_type(rhs.dens_type),
+  tipo_habitat(rhs.tipo_habitat),
+  birth_death_eq(rhs.birth_death_eq)
+  
+{
+  this->orientacao = runif(0,360);
+  
   if (rhs.genotype_mean.size()==1)
   {
     
@@ -146,33 +155,36 @@ individuo::individuo(const individuo& rhs)
   this->points=rhs.points;
 }
 
-/** \brief Método de atualização dos indivíduos 
+/** \brief Método de atualização dos indivíduos
  * Esta função é a camada de atualização dos indivíduos
  * A cada execução deste método:
  * - É atualizada a taxa de nascimento de acordo com a densidade de vizinhos no raio de vizinhanca obtido com paisagem::atualiza_vizinhos
  * - Sorteia o tempo de acordo com as novas taxas
  * As taxas de morte e movimentação no momento fixas. Mas tambem serão funções da densidade de vizinhos (\ref TBI).
  */
-
 void individuo::update(double dens)
 {
   double densi = dens; // Density of  neighbour individuals (includes focal one)
   
+  
   if (this->width_sd[0] == 0) {
-    if(this->tipo_habitat==0) // Checks if the individual is currently on the matrix
+    if(this->tipo_habitat==0) 
     {
-      this->birth = 0; // Sets birth rate to 0
-      
-      // To Do: Implementar aqui modelo mais geral para mortalidade na matriz. Aqui a denso dependencia é igual à do habitat, só muda a mortalidade basal que é maior que no habitat.
-      this->death = this->const_d_matrix*this->taxa_morte+this->incl_death*densi; // Sets the higer mortality rate atributed to nonhabita patches
+      this->birth = 0;
+      // ToDo: Implementar aqui modelo mais geral para mortalidade na matriz. Aqui a denso dependencia é igual à do habitat, só muda a mortalidade basal que é maior que no habitat.
+      this->death = this->const_d_matrix*this->taxa_morte+this->incl_death*densi; 
+      this->move = this->const_m_matrix*this->taxa_move;
     }
-    else
+    else 
     {
-      this->birth = this->taxa_basal-this->incl_birth*densi; // Computes the actual birth rate on habitat patch (that is influenced by the density of neighbours)
-      this->death = this->taxa_morte+this->incl_death*densi; // Computes the actual death rate on habitat patch (that is influenced by the density of neighbours)
+      this->birth = this->taxa_basal-this->incl_birth*densi;
+      this->death = this->taxa_morte+this->incl_death*densi;
+      this->move = this->taxa_move;
     }
   }
   else{
+    
+    //this->move = this->taxa_move;
     
     this->birth = this->taxa_basal-this->incl_birth*densi; // Computes the actual birth rate on habitat patch (that is influenced by the density of neighbours)
     this->death = ((this->const_d_matrix*this->taxa_morte)-((dnorm_sum(this->tipo_habitat, this->genotype_mean, this->width_sd)/dnorm(this->genotype_mean[0],this->genotype_mean[0],this-> width_sd[0]))*((this->const_d_matrix*this->taxa_morte)-this->taxa_morte))); // Computes the actual death rate on habitat patch (that is influenced by the suitability of its current habitat)
@@ -197,40 +209,39 @@ void individuo::sorteiaTempo()
 
 int individuo::sorteia_acao()
 {
-    vector<double> probscum;
-    double total = this->death+this->birth+this->move;
-    probscum.push_back(this->death/total);
-    probscum.push_back(probscum[0]+(this->birth/total)); 
-    probscum.push_back(probscum[1]+(this->move/total));  
-    double evento;
-    evento = runif(0.0,1.0); 
-
-    int decisao;
-	for(unsigned int i=0; i<probscum.size()-1; i++)
+  vector<double> probscum;
+  double total = this->death+this->birth+this->move;
+  probscum.push_back(this->death/total);
+  probscum.push_back(probscum[0]+(this->birth/total));
+  probscum.push_back(probscum[1]+(this->move/total));
+  double evento;
+  evento = runif(0.0,1.0);
+  
+  int decisao;
+  for(unsigned int i=0; i<probscum.size()-1; i++)
+  {
+    if(probscum[i]>evento)//encontrar o primeiro maior valor
     {
-        if(probscum[i]>evento)//encontrar o primeiro maior valor
-        {
-            decisao = i;
-            return decisao;//retorna a decisao tomada para o mundo (0 = morte, 1 = nascer, 2 = andar)
-			break;
-		}
-	}
-	return probscum.size()-1;
+      decisao = i;
+      return decisao;//retorna a decisao tomada para o mundo (0 = morte, 1 = nascer, 2 = andar)
+      break;
+    }
+  }
+  return probscum.size()-1;
 }
 
-void individuo::anda(bool aleatorio)
+void individuo::anda()
 {
-    //a cada movimentacao ele muda de direcao; random walk dentro de um angulo de visao
-	if (aleatorio) {
-		this->orientacao = runif(-180.0,180.0);//random way point para sortear uma direcao qualquer
-	} else {
-		this->orientacao+= runif(-ang_visada/2.0, ang_visada/2.0);//random way point
-	}
-    double oriRad=this->orientacao*M_PI/180.0;//tranforma em radianos para poder calcular as distancias das posicoes x e y
-    double dx= cos(oriRad)*this->passo;
-    double dy= sin(oriRad)*this->passo;
-    this->x+=dx;
-    this->y+=dy;
+  this->orientacao+= runif(-this->ang_visada/2.0, this->ang_visada/2.0);//random way point
+  if(this->orientacao < 0)
+    this->orientacao +=360;
+  else if(this->orientacao >= 360)
+    this->orientacao -= 360;
+  double oriRad=this->orientacao*M_PI/180.0;//tranforma em radianos para poder calcular as distancias das posicoes x e y
+  double dx= cos(oriRad)*this->passo;
+  double dy= sin(oriRad)*this->passo;
+  this->x+=dx;
+  this->y+=dy;
 }
 
 double individuo::dnorm(double x ,double mean, double sd){
